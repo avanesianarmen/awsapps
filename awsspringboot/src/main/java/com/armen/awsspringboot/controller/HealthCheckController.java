5e4f25f3b7c0d28dcb3a78bb28230c3ce1434e74
avanesianarmen <jones.andy@mail.ru>
2020-11-18 16:09:17 +0400

    Add health check

---
+++ awsspringboot/src/main/java/com/armen/awsspringboot/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.armen.awsspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/health")
+public class HealthCheckController {
+
+  @GetMapping("/{name}")
+  public String greeting(@PathVariable String name) {
+    return String.format("<b>Hello, %s </b> <p><i>Have a nice day ;)</i><p/>", name);
+  }
+
+}
